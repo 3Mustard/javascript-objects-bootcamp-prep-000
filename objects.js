1019b0d4acbdde411f06e410a85d10c4ec4db573
Matthew C <m23.cullen@gmail.com>
2019-07-19 11:53:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = {};
+var playlist = {StVincent: "birth in reverse"};
+
+function updatePlaylist(playlist,artist,song){
+  playlist.artist = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName;
+  return playlist;
+}
